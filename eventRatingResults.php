1800f15899b5e0fa90e0edafa2feccd289e14947
Wedderburn <wlesroy98@outlook.com>
2019-03-27 17:19:36 +0000

    Added new changes and meeting minutes

--- eventRatingResults.php
+++ eventRatingResults.php
@@ -8,11 +8,9 @@ mysqli_query($conn, $query) or die('Error querying database.');
 
 $result = mysqli_query($conn, $query);
 
-
 while ($row = mysqli_fetch_array($result) ) {
  echo $row['event_id'] . ' ' . $row['user_feedback'] . ': ' . $row['rating'] . ' ' . $row['user_email'] .'<br />';
 
  }
-
 CloseCon($conn);
 ?>
